66f7c361d4860639b66da65977a6032622cccde5
Aniket Jha <85429918+aniketjha349@users.noreply.github.com>
2023-08-19 00:33:30 +0530

    Add files via upload

---
+++ codsoft_calculator.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    global calculation
+    calculation +=str(symbol)
+    text_result.delete(1.0,"end")
+    text_result.insert(1.0,calculation)
+
+
+def evaluate_calculation():
+    global calculation
+    try:
+        result = str(eval(calculation))
+        calculation =""
+        text_result.delete(1.0,"end")
+        text_result.insert(1.0,result)
+    except:
+        clear_field()
+        text_result.insert(1.0,"Error")
+
+def clear_field():
+    global calculation
+    calculation =""
+    text_result.delete(1.0,"end")
+
+root = tk.Tk()
+root.geometry("300x275")
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+text_result.grid(columnspan=5)
+
+bin_1 = tk.Button(root, text="1",command=lambda:add_to_calculation(1), width=5, font=("Arial", 14))
+bin_1.grid(row=2,column=1)
+bin_2 = tk.Button(root, text="2",command=lambda:add_to_calculation(2), width=5, font=("Arial", 14))
+bin_2.grid(row=2,column=2)
+bin_3 = tk.Button(root, text="3",command=lambda:add_to_calculation(3), width=5, font=("Arial", 14))
+bin_3.grid(row=2,column=3)
+bin_4 = tk.Button(root, text="4",command=lambda:add_to_calculation(4), width=5, font=("Arial", 14))
+bin_4.grid(row=3,column=1)
+bin_5 = tk.Button(root, text="5",command=lambda:add_to_calculation(5), width=5, font=("Arial", 14))
+bin_5.grid(row=3,column=2)
+bin_6 = tk.Button(root, text="6",command=lambda:add_to_calculation(6), width=5, font=("Arial", 14))
+bin_6.grid(row=3,column=3)
+bin_7 = tk.Button(root, text="7",command=lambda:add_to_calculation(7), width=5, font=("Arial", 14))
+bin_7.grid(row=4,column=1)
+bin_8 = tk.Button(root, text="8",command=lambda:add_to_calculation(8), width=5, font=("Arial", 14))
+bin_8.grid(row=4,column=2)
+bin_9 = tk.Button(root, text="9",command=lambda:add_to_calculation(9), width=5, font=("Arial", 14))
+bin_9.grid(row=4,column=3)
+bin_0 = tk.Button(root, text="0",command=lambda:add_to_calculation(0), width=5, font=("Arial", 14))
+bin_0.grid(row=5,column=2)
+bin_plus = tk.Button(root, text="+",command=lambda:add_to_calculation("+"), width=5, font=("Arial", 14))
+bin_plus.grid(row=2,column=4)
+bin_minus = tk.Button(root, text="-",command=lambda:add_to_calculation("-"), width=5, font=("Arial", 14))
+bin_minus.grid(row=3,column=4)
+bin_mul = tk.Button(root, text="*",command=lambda:add_to_calculation("*"), width=5, font=("Arial", 14))
+bin_mul.grid(row=4,column=4)
+bin_div = tk.Button(root, text="/",command=lambda:add_to_calculation("/"), width=5, font=("Arial", 14))
+bin_div.grid(row=5,column=4)
+bin_close = tk.Button(root, text=")",command=lambda:add_to_calculation(")"), width=5, font=("Arial", 14))
+bin_close.grid(row=5,column=1)
+bin_open = tk.Button(root, text="(",command=lambda:add_to_calculation("("), width=5, font=("Arial", 14))
+bin_open.grid(row=5,column=3)
+bin_equal = tk.Button(root, text="=",command=evaluate_calculation, width=11, font=("Arial", 14))
+bin_equal.grid(row=6,column=3,columnspan=2)
+bin_clear = tk.Button(root, text="C",command=clear_field, width=11, font=("Arial", 14))
+bin_clear.grid(row=6,column=1,columnspan=2)
+
+root.mainloop()
